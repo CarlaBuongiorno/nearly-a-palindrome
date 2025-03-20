7c2fbec0a6ffb32e09a8416edff02c1a52de439c
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-03-20 11:56:57 +0100

    Add tests for eliminating input of less than 3 letters and checking a simple true palindrome

--- nearly_a_palindrome.py
+++ nearly_a_palindrome.py
@@ -1,2 +1,8 @@
 def check_palindrome(user_input):
-    pass
+    i = list(user_input)
+    if len(i) < 3:
+        return False
+    i.reverse()
+    if list(user_input) == i:
+        return True
+    return False
--- test_nearly_a_palindrome.py
+++ test_nearly_a_palindrome.py
@@ -3,4 +3,11 @@ from nearly_a_palindrome import check_palindrome
 
 def test_check_palindrome_exists():
     assert check_palindrome
-    
+
+
+def test_one_letter_palindrome_is_false():
+    assert check_palindrome('a') == False
+
+
+def test_three_letter_palindrome_is_true():
+    assert check_palindrome('aba') == True

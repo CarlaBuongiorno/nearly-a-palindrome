55d2e11307877a6e24ddd9f5528397841032cffc
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-03-20 11:27:37 +0100

    Initial set up, separate main and pure python files, add test file and .gitignore

--- nearly_a_palindrome.py
+++ nearly_a_palindrome.py
@@ -1,16 +1,2 @@
-'''Write a function that takes a string, and returns True if that string is a palindrome,
-or if it's nearly a palindrome. "Nearly a palindrome" is defined as "a palindrome with extra letters as the end."
-Minimum length of the palindrome part is 3 characters.
-
-Examples:
-
-poop -> True
-crap -> False
-Madam, I'm Adam -> True
-tent -> False
-tenet -> True
-tenets -> True (extra letters at the end, so it's nearly a palindrome)
-babble -> True (extra letters at the end, "bab" is a palindrome)
-levelling -> True (extra letters at the end)
-lotto -> False ("otto" is palindromic, but the extra letter is at the *start*, so it doesn't count.)
-aardvark -> False (i suppose "aa" is a palindrome, but it's too short to count.)'''
+def check_palindrome(user_input):
+    pass
---
+++ test_nearly_a_palindrome.py
@@ -0,0 +1,6 @@
+from nearly_a_palindrome import check_palindrome
+
+
+def test_check_palindrome_exists():
+    assert check_palindrome
+    

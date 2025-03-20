9646a7b8ee8c6e495f3c87614079072b2c49eac6
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-03-20 16:09:38 +0100

    Refactor check_palindrome() and refactor tests to use parametrize

--- nearly_a_palindrome.py
+++ nearly_a_palindrome.py
@@ -2,10 +2,15 @@ def check_palindrome(user_input):
     cleaned_input = _clean_input(user_input)
     reversed_input = cleaned_input[::-1]
     while reversed_input != cleaned_input:
-        cleaned_input.pop()
-        reversed_input.pop(0)
+        cleaned_input, reversed_input = _remove_excess_letters(cleaned_input, reversed_input)
     return cleaned_input == reversed_input and len(cleaned_input) >= 3
 
 
 def _clean_input(user_input):
     return list(user_input)
+
+
+def _remove_excess_letters(cleaned_input, reversed_input):
+        cleaned_input.pop()
+        reversed_input.pop(0)
+        return cleaned_input, reversed_input
--- test_nearly_a_palindrome.py
+++ test_nearly_a_palindrome.py
@@ -1,3 +1,5 @@
+import pytest
+
 from nearly_a_palindrome import check_palindrome
 
 
@@ -5,21 +7,12 @@ def test_check_palindrome_exists():
     assert check_palindrome
 
 
-def test_one_letter_palindrome_is_false():
-    assert check_palindrome('a') == False
-
-
-def test_three_letter_palindrome_is_true():
-    assert check_palindrome('aba') == True
-
-
-def test_five_letter_palindrome_is_true():
-    assert check_palindrome('tenet') == True
-
-
-def test_nearly_a_palindrome():
-    assert check_palindrome('tenets') == True
-
-
-def test_nearly_a_palindrome_too_small():
-    assert check_palindrome('aardvark') == False
+@pytest.mark.parametrize('user_input, expected', [
+    ('a',           False),
+    ('aba',         True),
+    ('tenet',       True),
+    ('tenets',      True),
+    ('aardvark',    False),
+])
+def test_nearly_a_palindrome(user_input, expected):
+    assert check_palindrome(user_input) == expected

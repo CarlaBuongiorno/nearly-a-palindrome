f652019d11108b6f4b2f3ff5ceb29fc1c821572a
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-03-24 16:58:52 +0100

    Remove redundant code in check_palindrome()

--- nearly_a_palindrome.py
+++ nearly_a_palindrome.py
@@ -3,7 +3,7 @@ def check_palindrome(user_input):
     reversed_input = cleaned_input[::-1]
     while reversed_input != cleaned_input:
         cleaned_input, reversed_input = _remove_excess_letters(cleaned_input, reversed_input)
-    return cleaned_input == reversed_input and len(cleaned_input) >= 3
+    return len(cleaned_input) >= 3
 
 
 def _clean_input(user_input):
